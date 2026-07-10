30939d47f087a3d615e1291d8e6dab559d9797b3
Kuraishi Taiki <kuraishi.taiki0@gmail.com>
2026-07-10 23:45:48 +0900

    Revert "✨ feat(llama-cpp): add Gemma4 QAT model and TurboQuant trial pod (#577)"
    This reverts commit 91274359d3a100fa80c3704f2ab92cf11fbbbc72.

--- infra/modules/dns/main.tf
+++ infra/modules/dns/main.tf
@@ -58,15 +58,6 @@ resource "cloudflare_dns_record" "llama_cpp" {
   ttl     = 1
 }
 
-resource "cloudflare_dns_record" "llama_cpp_turboquant" {
-  zone_id = data.cloudflare_zone.this.zone_id
-  name    = "llama-cpp-turboquant.${var.zone_name}"
-  type    = "CNAME"
-  content = var.tunnel_cname
-  proxied = true
-  ttl     = 1
-}
-
 resource "cloudflare_dns_record" "zot" {
   zone_id = data.cloudflare_zone.this.zone_id
   name    = "zot.${var.zone_name}"
